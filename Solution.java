4e9b13431d9e348f97ebfc856d6b27fce0a59a60
KimYongHyeon <kkskww@gmail.com>
2019-02-12 13:32:33 +0000

    Update Solution.java

---
+++ Solution.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Solution{
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		String str;
+		StringTokenizer token;
+
+		int TC = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case < TC; test_case++) {
+
+			sb.append("#").append(test_case).append(" ").append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+}
