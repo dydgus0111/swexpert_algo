28c43463a19b7707a469118e65c062198deeb87b
KimYongHyeon <kkskww@gmail.com>
2019-02-14 05:32:41 +0000

    Update D5/6782.java

--- D5/6782.java
+++ D5/6782.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
   
-public class Solution{
+public class Solution6782{
     public static void main(String[] args) throws IOException{
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
