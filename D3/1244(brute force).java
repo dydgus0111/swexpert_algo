c66b2fe3b2f4309f779a33a73e25c92eba495757
KimYongHyeon <kkskww@gmail.com>
2019-02-14 02:31:46 +0000

    Update D3/1244(brute force).java

--- D3/1244(brute force).java
+++ D3/1244(brute force).java
@@ -1,71 +1,57 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
-public class Solution1244{
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static void swap(int[] arr, int a, int b) {
-		int temp=arr[a];
-		arr[a]=arr[b];
-		arr[b]=temp;
-	}
-	static int []ans;
-	static void shuffle(int[] arr, int cnt,int N,int []max) {
-		if(cnt==N) {
-			for(int i=0;i<max.length;i++) {
-				if(max[i]==arr[i]) {
-					continue;
-				}else if(max[i]<arr[i]) {
-					for(int j=0;j<max.length;j++) {
-						max[j]=arr[j];
-					}
-					break;
-				}else if(max[i]>arr[i])
-					break;
-			}
-			for(int i=0;i<max.length;i++) {
-				ans[i]=max[i];
-			}
-			return;
-		}
-		for(int i=0;i<arr.length;i++) {
-			for(int j=i+1;j<arr.length;j++) {
-				swap(arr,i,j);
-				shuffle(arr, cnt+1, N,max);
-				swap(arr,i,j);
-			}
-		}
-	}
-	public static void main(String[] args) throws IOException {
-		StringTokenizer token;
-		int TC = Integer.parseInt(br.readLine());
-		for (int test_case = 1; test_case <= TC; test_case++) {
-			String str=br.readLine();
-			token=new StringTokenizer(str);
-			String input=token.nextToken();
-			int num=Integer.parseInt(token.nextToken());
-			int []arr=new int[input.length()];
-			for(int i=0;i<input.length();i++) {
-				arr[i]=input.charAt(i)-'0';
-			}
-			int []max=new int[input.length()];
-			ans=new int[input.length()];
-			shuffle(arr, 0, num, max);
-			StringBuilder sb = new StringBuilder();
-			StringBuilder sb1=new StringBuilder();
-			for(int i=0;i<ans.length;i++) {
-				sb1.append(ans[i]);
-			}
-			sb.append("#").append(test_case).append(" ").append(sb1.toString()).append("\n");
-			bw.write(sb.toString());
-		}
-
-		bw.flush();
-		bw.close();
-	}
-
+import java.util.HashSet;
+import java.util.Scanner;
+ 
+public class Solution1244_완전 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int tc = 1; tc <= T; tc++) {
+            String s = sc.next();
+            int N = sc.nextInt();
+            int[] num = new int[s.length()];
+            for(int i = 0; i < s.length(); i++) {
+                num[i] = s.charAt(i) - '0';
+            }
+            max = 0;
+            visited = new HashSet<>();
+            solve(num, 0, N);
+            System.out.println(max);
+             
+        }
+    }
+    static HashSet<String> visited = new HashSet<>();
+    static int max = 0;
+    static void solve(int[] num, int cnt, int N) {
+        int sum = 0;
+        for(int i = 0; i < num.length; i++) {
+            sum = sum*10 + num[i];
+        }
+        if( visited.contains(sum + cnt + "")) {
+            return;
+        }
+        else {
+            visited.add(sum + cnt + "");
+        }
+         
+        if( cnt == N ) {
+            //3, 2, 4, 5
+             
+            if( max < sum )
+                max = sum;
+            return;
+        }
+         
+        for(int i = 0; i < num.length; i++) {
+            for(int j = i + 1; j < num.length; j++) {
+                swap(num, i, j);
+                solve(num, cnt+1, N);
+                swap(num, i, j);
+            }
+        }
+    }
+    static void swap(int[] arr , int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
 }
