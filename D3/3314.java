e16c3a64e885346ac12a2e9f1c304717635a4db1
KimYongHyeon <kkskww@gmail.com>
2019-02-27 01:42:10 +0000

    Update D3/3314.java

---
+++ D3/3314.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Solution3314{
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		StringTokenizer token;
+		int TC = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= TC; test_case++) {
+			token=new StringTokenizer(br.readLine());
+			int sum=0;
+			int cnt=0;
+			while(token.hasMoreTokens()) {
+				int score=Integer.parseInt(token.nextToken());
+				if(score<40)
+					score=40;
+				sum+=score;
+				cnt++;
+			}
+			int ans=sum/cnt;
+			StringBuilder sb = new StringBuilder();
+			sb.append("#").append(test_case).append(" ").append(ans).append("\n");
+			bw.write(sb.toString());
+		}
+
+		bw.flush();
+		bw.close();
+	}
+
+}
